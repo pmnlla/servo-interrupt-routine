3bf8a9c1c577fbd4db0e1f64ee7c7e640c165167
pmnlla <9027165+pmnlla@users.noreply.github.com>
2024-02-16 12:28:31 -0500

    push a little bit more

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
+#![feature(trivial_bounds)]
 
 // arduino and atmega hal
 use arduino_hal::port::mode;
@@ -9,28 +11,31 @@ use panic_halt as _;
 use core::{borrow::Borrow, cell};
 use heapless::Vec;
 
-static TIME_SINCE_NEW_CYCLE: avr_device::interrupt::Mutex<cell::Cell<i32>> =
+static TIME_SINCE_NEW_CYCLE: avr_device::interrupt::Mutex<cell::Cell<u32>> =
     avr_device::interrupt::Mutex::new(cell::Cell::new(0));
 
 struct LocalPin {
-    value: u8,
+    value: u32,
     port: arduino_hal::port::Pin<mode::Output>
 }
 
 impl ServoController{
-    pub fn ConvertValueToCyclePeriod(i: u8) -> u8 {
-        i
+    pub fn ConvertValueToCyclePeriod(i: u32) -> u32 {
+        i // todo: make this the actual cycle value period conversion
     }
     pub fn ToggleAllStates(self) {
         for mut i in self.pinPortVector {
             if 
                 avr_device::interrupt::free(|cs| {TIME_SINCE_NEW_CYCLE.borrow(cs).get()})
-            <= Self::ConvertValueToCyclePeriod(i.value) as i32 {
+            <= Self::ConvertValueToCyclePeriod(i.value) {
                 i.port.set_high();
             } else {
                 i.port.set_low();
             }
         }
+    }
+    pub fn AddServoToArr(mut self, v: LocalPin) {
+        self.pinPortVector.push(v);
     }
 }
 
@@ -39,7 +44,7 @@ struct ServoController{
     timeSinceLastTick: u8,
 }
 
-const fn GenSC() -> ServoController{
+const fn gen_sc() -> ServoController{
     let mut tvec: Vec<LocalPin, 10> = Vec::new();
     let mut tempsc = ServoController {
         pinPortVector: tvec,
@@ -50,17 +55,15 @@ const fn GenSC() -> ServoController{
 
 // storage - values and types
 const PRESCALER: u32 = 8;
-const TIMER_COUNTS: u32 = 125;
+const TIMER_COUNTS: u32 = 125; // each cycle occurs at 0.0625ms  past the last
 
 const MILLIS_INCREMENT: u32 = PRESCALER * TIMER_COUNTS / 16000;
 
 static SERVO_CONTROLLER_CONTAINER: avr_device::interrupt::Mutex<cell::Cell<ServoController>> =
-    avr_device::interrupt::Mutex::new(cell::Cell::new(GenSC()));
-
-
+    avr_device::interrupt::Mutex::new(cell::Cell::new(gen_sc()));
 
 // timer code - timer will initialize the servocontroller et al
-fn millis_init(tc0: arduino_hal::pac::TC0) {
+fn timer_init(tc0: arduino_hal::pac::TC0) {
     // Configure the timer for the above interval (in CTC mode)
     // and enable its interrupt.
     tc0.tccr0a.write(|w| w.wgm0().ctc());
@@ -74,26 +77,42 @@ fn millis_init(tc0: arduino_hal::pac::TC0) {
     });
     tc0.timsk0.write(|w| w.ocie0a().set_bit());
 
-    /* Reset the global millisecond counter
+    // Reset the global millisecond counter
     avr_device::interrupt::free(|cs| {
-        MILLIS_COUNTER.borrow(cs).set(0);
-    });*/
+        TIME_SINCE_NEW_CYCLE.borrow(cs).set(0);
+    });
+}
+
+#[avr_device::interrupt(atmega328p)]
+fn TIMER0_COMPA() {
+    avr_device::interrupt::free(|cs| {
+        let counter_cell = TIME_SINCE_NEW_CYCLE.borrow(cs);
+        let counter = counter_cell.get();
+        counter_cell.set(counter + MILLIS_INCREMENT);
+    })
+}
+
+fn attach(i: LocalPin)
+where
+ServoController: core::marker::Copy,
+ServoController: Clone{
+    avr_device::interrupt::free(|cs| {
+        let counter_cell = SERVO_CONTROLLER_CONTAINER.borrow(cs);
+        let counter = counter_cell.get();
+        //counter_cell.set()
+    })
 }
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    timer_init(dp.TC0);
+
+    // Enable interrupts globally
+    unsafe { avr_device::interrupt::enable() };
 
     let mut led = pins.d13.into_output();
 
